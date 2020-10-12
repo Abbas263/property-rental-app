6448dca65ef9fea7e88ccec2be5d28d298f259f8
Abbas Ali <abbas.ali5788@gmail.com>
2020-10-12 09:10:48 -0700

    initialize-project

---
+++ frontend/src/app/property/property-list/property-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+selector :"app-property-list",
+templateUrl:"./property-list.component.html"
+})
+
+export class PropertyListComponent{
+
+}
